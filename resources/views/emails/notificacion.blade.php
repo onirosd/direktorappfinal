fbe19c26c46740248f8739f8e098e444db16f472
onirosd <diegowarthon1190@gmail.com>
2023-03-27 00:04:07 -0500

    subimos cambios

--- resources/views/emails/notificacion.blade.php
+++ resources/views/emails/notificacion.blade.php
@@ -54,129 +54,115 @@ a[x-apple-data-detectors] {
 				<v:fill type="tile" color="#ffffff"></v:fill>
 			</v:background>
 		<![endif]-->
-   <table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;padding:0;Margin:0;width:100%;height:100%;background-repeat:repeat;background-position:center top;background-color:#FFFFFF">
-     <tr>
-      <td valign="top" style="padding:0;Margin:0">
-       <table cellpadding="0" cellspacing="0" class="es-content" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%">
-         <tr>
-          <td align="center" style="padding:0;Margin:0">
-           <table bgcolor="#efefef" class="es-content-body" align="center" cellpadding="0" cellspacing="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:#EFEFEF;border-radius:20px 20px 0 0;width:800px">
-             <tr>
-              <td align="left" style="padding:0;Margin:0;padding-top:40px;padding-left:40px;padding-right:40px">
-               <table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
-                 <tr>
-                  <td align="left" valign="top" style="padding:0;Margin:0;">
+        <table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;padding:0;Margin:0;width:100%;height:100%;background-repeat:repeat;background-position:center top;background-color:#FFFFFF">
+      <tr>
+        <td valign="top" style="padding:0;Margin:0">
+        <table cellpadding="0" cellspacing="0" class="es-content" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%">
+            <tr>
+              <td align="center" style="padding:0;Margin:0">
+                 <table bgcolor="#efefef" class="es-content-body" align="center" cellpadding="0" cellspacing="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:#EFEFEF;border-radius:20px 20px 0 0;width:800px">
+                    <tr>
+                  <td align="left" style="padding:0;Margin:0;padding-top:40px;padding-left:40px;padding-right:40px">
+                  <table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
+                    <tr>
+                      <td align="left" valign="top" style="padding:0;Margin:0;">
 
-                     <a href="{{$des_link}}">
-                      <img src="{{$des_direktor_icon}}" alt="Confirm email" style="width: 200px; display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic;border-radius:0px" title="Confirm email">
-                     </a>
-                  </td>
-                    <td align="left" style="padding:20px;Margin:0;width:520px">
+                        <a href="{{$des_link}}">
+                          <img src="{{$des_direktor_icon}}" alt="Confirm email" style="width: 200px; display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic;border-radius:0px" title="Confirm email">
+                        </a>
+                      </td>
+                        <td align="left" style="padding:20px;Margin:0;width:520px">
 
-                      <h3 style="Margin:0;line-height:34px;mso-line-height-rule:exactly;font-family:'open sans', 'helvetica neue', helvetica, arial, sans-serif;font-size:28px;font-style:normal;font-weight:bold;color:#2D3142">
-                        Se alertan modificaciones en el proyecto {{$des_proyecto}}<br>
+                          <h3 style="Margin:0;line-height:34px;mso-line-height-rule:exactly;font-family:'open sans', 'helvetica neue', helvetica, arial, sans-serif;font-size:28px;font-style:normal;font-weight:bold;color:#2D3142">
+                            Se alertan modificaciones en el proyecto {{$des_proyecto}}<br>
 
-                      </h3>
-
-                   </td>
-                 </tr>
-               </table></td>
-             </tr>
-             <tr>
-              <td align="left" style="padding:0;Margin:0;padding-top:20px;padding-left:40px;padding-right:40px">
-               <table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
-                 <tr>
-                  <td align="center" valign="top" style="padding:0;Margin:0;width:520px">
-                   <table cellpadding="0" cellspacing="0" width="100%" bgcolor="#fafafa" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:separate;border-spacing:0px;background-color:#fafafa;border-radius:10px" role="presentation">
-                     <tr>
-                      <td align="left" style="padding:20px;Margin:0"><h3 style="Margin:0;line-height:34px;mso-line-height-rule:exactly;font-family:'open sans', 'helvetica neue', helvetica, arial, sans-serif;font-size:28px;font-style:normal;font-weight:bold;color:#2D3142">
-                        Hola,&nbsp;{{$des_correo}}</h3>
-
-                        <p style=" Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;font-family:'open sans', 'helvetica neue', helvetica, arial, sans-serif;line-height:27px;color:#2D3142;font-size:16px">
-                         Se han hecho modificacion en el analisis de restricciones del proyecto en mención , favor de revisar el detalle de las actualizaciones.
-                        </p>
-                        <br>
-                        <table border="0" bordercolor="#cccccc" align="center" class="es-table">
-                          <tbody>
-                            <tr style="background-color: #0062ae; height:50px; color: #fff;">
-
-                              <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Frente</td>
-                              <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Fase</td>
-
-                              <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Actividad</td>
-                              <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Restricción</td>
-                              <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Tipo</td>
-                              <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Fecha Requerida</td>
-                              <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Fecha Conciliada</td>
-                              <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Responsable</td>
-                              <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Area Responsable</td>
-                              <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Solicitante</td>
-
-                            </tr>
-                            <?php  foreach ($actividades as $key => $value) { ?>
-
-                            <tr style="height:50px">
-                              <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">{{$value->Frente}}</td>
-                              <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">{{$value->Fase}}</td>
-                              <td style="text-align: center; font-family: manrope, arial, sans-serif; font-size: 12px;">{{$value->Actividad}}</td>
-                              <td style="text-align: center; font-family: manrope, arial, sans-serif; font-size: 12px;">{{$value->Restriccion}}</td>
-                              <td style="text-align: center; font-family: manrope, arial, sans-serif; font-size: 12px;">{{$value->Tipo_Restriccion}}</td>
-                              <td style="text-align: center; font-family: manrope, arial, sans-serif; font-size: 12px;">{{$value->dayFechaRequerida}}</td>
-                              <td style="text-align: center; font-family: manrope, arial, sans-serif; font-size: 12px;">{{$value->dayFechaConciliada}}</td>
-                              <td style="text-align: center; font-family: manrope, arial, sans-serif; font-size: 12px;">{{$value->Usuario_Responsable}}</td>
-                              <td style="text-align: center; font-family: manrope, arial, sans-serif; font-size: 12px;">{{$value->Area_Responsable}}</td>
-                              <td style="text-align: center; font-family: manrope, arial, sans-serif; font-size: 12px;">{{$value->Usuario_Solicitante}}</td>
-
-                        </tr>
-
-                            <?php } ?>
-
-                           </tr>
-                          </tbody>
-                        </table>
-
-
+                          </h3>
 
                       </td>
-                     </tr>
-                   </table></td>
-                 </tr>
-               </table></td>
-             </tr>
-           </table></td>
-         </tr>
-       </table>
+                    </tr>
+                  </table></td>
 
-       <table cellpadding="0" cellspacing="0" class="es-content" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%">
-         <tr>
-          <td align="center" style="padding:0;Margin:0">
-           <table bgcolor="#efefef" class="es-content-body" align="center" cellpadding="0" cellspacing="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:#EFEFEF;border-radius:0 0 20px 20px;width:600px">
-             <tr>
-              <td class="esdev-adapt-off" align="left" style="Margin:0;padding-top:20px;padding-bottom:20px;padding-left:40px;padding-right:40px">
-               <table cellpadding="0" cellspacing="0" class="esdev-mso-table" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;width:895px">
-                 <tr>
-                  <td class="esdev-mso-td" valign="top" style="padding:0;Margin:0">
-                   <table cellpadding="0" cellspacing="0" align="left" class="es-left" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;float:left">
-                     <tr>
-                      <td align="center" valign="top" style="padding:0;Margin:0;width:47px">
-                       <table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
-                         <tr>
-                          <td align="center" style="padding:0;Margin:0;display:none"></td>
-                         </tr>
-                       </table></td>
-                     </tr>
-                   </table></td>
-                  <td style="padding:0;Margin:0;width:20px"></td>
-                  <td class="esdev-mso-td" valign="top" style="padding:0;Margin:0">
-                    <p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:'open sans', 'helvetica neue', helvetica, arial, sans-serif;line-height:24px;color:#2D3142;font-size:16px">Mensaje automatico , favor de no responder a este correo.</p>
+                    </tr>
+                    <tr>
+                  <td align="left" style="padding:0;Margin:0;padding-top:20px;padding-left:40px;padding-right:40px">
+                  <table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
+                    <tr>
+                      <td align="center" valign="top" style="padding:0;Margin:0;width:520px">
+                      <table cellpadding="0" cellspacing="0" width="100%" bgcolor="#fafafa" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:separate;border-spacing:0px;background-color:#fafafa;border-radius:10px" role="presentation">
+                              <tr>
+                          <td align="left" style="padding:20px;Margin:0"><h3 style="Margin:0;line-height:34px;mso-line-height-rule:exactly;font-family:'open sans', 'helvetica neue', helvetica, arial, sans-serif;font-size:28px;font-style:normal;font-weight:bold;color:#2D3142">
+                            Hola,&nbsp;{{$des_correo}}</h3>
 
-                  </td>
-                 </tr>
-               </table></td>
-             </tr>
-           </table></td>
-         </tr>
-       </table>
+                            <p style=" Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;font-family:'open sans', 'helvetica neue', helvetica, arial, sans-serif;line-height:27px;color:#2D3142;font-size:16px">
+                            Se han hecho modificacion en el analisis de restricciones del proyecto en mención , favor de revisar el detalle de las actualizaciones.
+                            </p>
+                            <br>
+                            <table border="0" bordercolor="#cccccc" align="center" class="es-table">
+                              <tbody>
+                                <tr style="background-color: #0062ae; height:50px; color: #fff;">
+
+                                  <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Frente</td>
+                                  <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Fase</td>
+
+                                  <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Actividad</td>
+                                  <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Restricción</td>
+                                  <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Tipo</td>
+                                  <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Fecha Requerida</td>
+                                  <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Fecha Conciliada</td>
+                                  <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Responsable</td>
+                                  <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Area Responsable</td>
+                                  <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">Solicitante</td>
+
+                                </tr>
+                                <?php  foreach ($actividades as $key => $value) { ?>
+
+                                <tr style="height:50px">
+                                  <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">{{$value->Frente}}</td>
+                                  <td style="font-family: manrope, arial, sans-serif; text-align: center; font-size: 14px;">{{$value->Fase}}</td>
+                                  <td style="text-align: center; font-family: manrope, arial, sans-serif; font-size: 12px;">{{$value->Actividad}}</td>
+                                  <td style="text-align: center; font-family: manrope, arial, sans-serif; font-size: 12px;">{{$value->Restriccion}}</td>
+                                  <td style="text-align: center; font-family: manrope, arial, sans-serif; font-size: 12px;">{{$value->Tipo_Restriccion}}</td>
+                                  <td style="text-align: center; font-family: manrope, arial, sans-serif; font-size: 12px;">{{$value->dayFechaRequerida}}</td>
+                                  <td style="text-align: center; font-family: manrope, arial, sans-serif; font-size: 12px;">{{$value->dayFechaConciliada}}</td>
+                                  <td style="text-align: center; font-family: manrope, arial, sans-serif; font-size: 12px;">{{$value->Usuario_Responsable}}</td>
+                                  <td style="text-align: center; font-family: manrope, arial, sans-serif; font-size: 12px;">{{$value->Area_Responsable}}</td>
+                                  <td style="text-align: center; font-family: manrope, arial, sans-serif; font-size: 12px;">{{$value->Usuario_Solicitante}}</td>
+
+                            </tr>
+
+                                <?php } ?>
+
+                              </tr>
+                              </tbody>
+                            </table>
+
+
+
+                        </td>
+                      </tr>
+                      <!-- <tr>
+                        <td> veremos</td>
+                      </tr> -->
+                      </table>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td align="center">
+                        <br>
+                        <p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:'open sans', 'helvetica neue', helvetica, arial, sans-serif;line-height:24px;color:#2D3142;font-size:16px">Mensaje automatico , favor de no responder a este correo.</p>
+                        <br>
+                      </td>
+                    </tr>
+                  </table>
+              </td>
+            </tr>
+            </table></td>
+          </tr>
+        </table>
+
+
+
+
        <table cellpadding="0" cellspacing="0" class="es-footer" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%;background-color:transparent;background-repeat:repeat;background-position:center top">
          <tr>
           <td align="center" style="padding:0;Margin:0">
@@ -196,9 +182,12 @@ a[x-apple-data-detectors] {
              </tr>
            </table></td>
          </tr>
-       </table></td>
+       </table>
+
+      </td>
      </tr>
    </table>
   </div>
  </body>
 </html>
+
