46cfd7d4676d123fba2c44b8006e42f159b6f37e
onirosd <diegowarthon1190@gmail.com>
2024-05-30 10:19:13 -0500

    Subimos cambios para corregir bug de envio de correo masivo.

--- app/Jobs/SendEmails.php
+++ app/Jobs/SendEmails.php
@@ -19,18 +19,64 @@ class SendEmails implements ShouldQueue
 
     }
 
+    // public function handle()
+    // {
+    //     $correosPendientes = conf_colacorreos::whereNull('dayFechaEnvio')->get();
+    //     foreach ($correosPendientes as $correo) {
+    //         $correosBCC = json_decode($correo->desCorreosBCC, true);
+
+    //         Mail::send('emails.emailTemplate', ['content' => $correo->desMensaje], function ($message) use ($correo, $correosBCC) {
+    //             $message->to($correo->desCorreoEnvio);  // Correo principal
+    //             $message->subject($correo->desMotivo);
+
+    //             if (!empty($correosBCC)) {
+    //                 foreach ($correosBCC as $bcc) {
+    //                     $message->bcc($bcc);  // Agregar correos BCC
+    //                 }
+    //             }
+    //         });
+
+    //         // $correo->update(['dayFechaEnvio' => now()]);
+    //         conf_colacorreos::where('codColaCorreo',$correo->codColaCorreo)->update(["dayFechaEnvio" => date('Y-m-d H:i:s')]);
+    //     }
+    // }
+
+
     public function handle()
-    {
-        $results = conf_colacorreos::whereNull('dayFechaEnvio')->get();
-        if($results){
-            foreach ($results as $key => $mail){
-                Mail::to($mail->desCorreoEnvio)->send(new InvitationEmail($mail->desMensaje, $mail->desMotivo));
-                $insertMail = conf_colacorreos::where('codColaCorreo',$mail->codColaCorreo)->update([
-                    "dayFechaEnvio" => date('Y-m-d H:i:s')
-                ]);
-            }
-        }
+{
+    $results = conf_colacorreos::whereNull('dayFechaEnvio')->get();
+    foreach ($results as $mail) {
+        $correosBCC = json_decode($mail->desCorreosBCC, true); // Asume que los correos BCC están almacenados en formato JSON en la columna desCorreosBCC
+
+        // Crear una instancia del Mailable
+        $email = new InvitationEmail($mail->desMensaje, $mail->desMotivo);
+
+        // Configurar el destinatario principal y los BCC
+        Mail::to($mail->desCorreoEnvio)
+            ->bcc($correosBCC) // Añadir BCC si existen
+            ->send($email);
+
+        // Actualizar el registro para marcarlo como enviado
+        conf_colacorreos::where('codColaCorreo', $mail->codColaCorreo)->update([
+            "dayFechaEnvio" => now()
+        ]);
     }
+}
+
+    // public function handle()
+    // {
+    //     $results = conf_colacorreos::whereNull('dayFechaEnvio')->get();
+    //     if($results){
+    //         foreach ($results as $key => $mail){
+    //             Mail::to($mail->desCorreoEnvio)->send(new InvitationEmail($mail->desMensaje, $mail->desMotivo));
+    //             $insertMail = conf_colacorreos::where('codColaCorreo',$mail->codColaCorreo)->update([
+    //                 "dayFechaEnvio" => date('Y-m-d H:i:s')
+    //             ]);
+    //         }
+    //     }
+    // }
+
+
 }
 
 
--- config/global.php
+++ config/global.php
@@ -6,10 +6,9 @@ return [
     'URL'               => $_ENV['CLIENT_SIDE_URL'],
     'URL_CHANGE_CREDEN' => $_ENV['CLIENT_SIDE_URL']."/cambiarcreden",
 
+    'MAIL_FROM_ADDRESS'=> $_ENV['MAIL_FROM_ADDRESS'],
+
     // 'ICON_DIREKTOR' => $_ENV['CLIENT_SIDE_URL']."/src/assets/logo.png"
-
-
-
     'ICON_DIREKTOR' => 'https://direktor.com.pe/assets/isotipoD.png'
     // 'ICON_DIREKTOR' => $_ENV['CLIENT_SIDE_URL'].'/src/assets/isotipoD.png'
 
